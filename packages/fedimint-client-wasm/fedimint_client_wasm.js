d5bef8f4f47dce8e28b548ffdd78e2ca910879be
Alex Lewin <alexlwn123@gmail.com>
2024-07-30 17:13:15 -0400

    refactor: converted to bun monorepo

--- packages/fedimint-client-wasm/fedimint_client_wasm.js
+++ packages/fedimint-client-wasm/fedimint_client_wasm.js
@@ -898,7 +898,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = "/fedimint-ts/assets/fedimint_client_wasm_bg.wasm"
+        input = new URL('fedimint_client_wasm_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
